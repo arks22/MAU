28143c990c79dafbd4a22a2cdb7bdf845efa9a8a
arks22 <sasaki.10222022@gmail.com>
2022-08-06 03:06:08 +0900

    add run_schedule etc

--- docker_run.sh
+++
@@ -1,1 +0,0 @@
-docker run --rm --name "sasaki_mau" --gpus all -v /home/sasaki/MAU:/home/MAU -it --shm-size=1024mb sasaki/mau:cuda116
--- run_schedule.sh
+++
@@ -1,9 +0,0 @@
-python3 MAU_run.py --dataset=mnist --config=mnist --batch_size=8
-python3 MAU_run.py --dataset=mnist --config=mnist --batch_size=16
-python3 MAU_run.py --dataset=mnist --config=mnist --batch_size=32
-python3 MAU_run.py --dataset=mnist --config=mnist --batch_size=64
-python3 MAU_run.py --dataset=mnist --config=mnist --batch_size=96
-python3 MAU_run.py --dataset=aia211_5 --config=aia211_5 --batch_size=1
-python3 MAU_run.py --dataset=aia211_5 --config=aia211_5 --batch_size=2
-python3 MAU_run.py --dataset=aia211_5 --config=aia211_5 --batch_size=4
-python3 MAU_run.py --dataset=aia211_5 --config=aia211_5 --batch_size=8
